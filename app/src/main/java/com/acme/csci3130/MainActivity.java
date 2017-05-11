b47e4cf3c40f63973210da423eda42c0dcaddd8c
DESKTOP-7846H8F\Mishal <alnajdi.mishal@gmail.com>
2017-05-10 21:30:38 -0300

    Error

--- app/src/main/java/com/acme/csci3130/MainActivity.java
+++ app/src/main/java/com/acme/csci3130/MainActivity.java
@@ -1,13 +1,28 @@
 package com.acme.csci3130;
 
-import android.app.Activity;
-import android.os.Bundle;
 
-public class MainActivity extends Activity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText x;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void submit (View view){
+        x = (EditText) findViewById(R.id.editText2);
+        TextView y = (TextView) findViewById(R.id.helloText);
+        y.setText(x.getText());
     }
 }
